d25769bdaeeee80e07bdcb3f68b6ec5d309db1ce
dirimaximus-programmer <272503574+dirimaximus-programmer@users.noreply.github.com>
2026-06-23 09:56:35 +0300

    Create credits.bat
    Micro-update. Don't ready

---
+++ credits.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001 >nul
+echo =========
+echo  THANKS!
+echo =========
+timeout /t 1
+echo ============MAIN BRANCH============
+echo Main maker: dirimaximus-programmer
+echo Code helper: Google Gemini [AI HELP]
+echo ===================================
+echo
