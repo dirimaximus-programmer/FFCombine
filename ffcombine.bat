bcf339039bb208a428fee33558a7458bd8e1ec69
dirimaximus-programmer <272503574+dirimaximus-programmer@users.noreply.github.com>
2026-06-23 09:50:24 +0300

    Update ffcombine.bat
    Updating menu - credits added

--- ffcombine.bat
+++ ffcombine.bat
@@ -11,17 +11,20 @@ echo ======================================================
 echo          FFCOMBINE v1.0
 echo ======================================================
 echo  1. MEDIA (FFmpeg: View, Analyze, Convert)
-echo  2. AI NEURAL NETWORK (Llama.cpp: Chat, Server)
-echo  3. CODE BUILDER (MinGW64 GCC / G++ Environment)
-echo  4. CODE BUILDER (Pure Clang / LLVM Cross-Compiler)
-echo  5. NETWORK (YT-DLP: Advanced Media Downloader)
-echo  6. NETWORK (Wget: Fast URL File Downloader)
+echo  2. AI LLM (Llama.cpp: Chat, Server)
+echo  3. GCC CODE BUILDER (MinGW64 GCC / G++ Environment)
+echo  4. LLVM CODE BUILDER (Pure Clang / LLVM Cross-Compiler)
+echo  5. YT-DOWNLIADER (YT-DLP: Advanced Media Downloader)
+echo  6. URL DOWNLOADER (Wget: Fast URL File Downloader)
 echo  7. AUTO-GIT (Bash / CMD Environment Selector)
 echo  8. Exit
+echo. 9. Credits
 echo ======================================================
 echo.
 
-choice /c 12345678 /n /m "Select tool (1-8): "
+choice /c 123456789 /n /m "Select tool (1-9): "
+
+if errorlevel 9 call .\credits.bat & goto main
 if errorlevel 8 exit
 if errorlevel 7 call .\git-combine.bat & goto main
 if errorlevel 6 call .\wget-combine.bat & goto main
